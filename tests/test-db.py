3a378e10366e1761e31c44907cac67c402d3b93d
Clint Campbell <45945445+cclint@users.noreply.github.com>
2024-01-05 10:26:11 -0900

    Delete tests/test-db.py

--- tests/test-db.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from unittest.mock import MagicMock
-from db import get_connection, get_cursor, get_all_trails, add_trail
-
-@pytest.fixture
-def mock_psycopg2_connection():
-    mock_conn = MagicMock()
-    return mock_conn
