9194cb515c17ba7931b2e3b6c8386128ef5c13d7
Jmartz007 <109188512+Jmartz007@users.noreply.github.com>
2024-01-19 13:13:04 -0900

    Update app.py to work on container
    this is my sole contribution to this project

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def about_page():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000, host='0.0.0.0')
