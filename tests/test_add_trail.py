5fa2caab42b7d26664c673290b5b20841021ee51
Miller <crmiller@dot.soa.alaska.gov>
2024-01-25 15:04:59 -0900

    Selenium tests on Hiking APIgit status!

--- tests/test_add_trail.py
+++ tests/test_add_trail.py
@@ -2,25 +2,40 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from chromedriver_py import binary_path
+import pytest
 import time
 
-def test_page_titles_are_correct():
+@pytest.fixture(scope="module")
+def browser_session():
     svc = Service(binary_path)
-    print(binary_path)
     browser = webdriver.Chrome(service=svc)
-
-    browser.get('https://www.google.com/')
-
-    search_bar = browser.find_element(By.NAME,"q")
-    search_bar.send_keys("Poop Images")
-    search_bar.submit()
-    time.sleep(2)
-    
-    images_link = browser.find_element(By.LINK_TEXT, "Images")
-    images_link.click
-    time.sleep(10)
-
-
+    browser.implicitly_wait(1)
+    yield browser
     browser.quit()
 
-test_page_titles_are_correct()
+@pytest.mark.parametrize("input_name, input_location, input_description", [
+    ("Hatcher's Pass", "Palmer, AK", "Super Fun"),
+    ("Thunderbird Falls", "Chugiak, AK", "The waterfalls are really neat LOL"),
+    ("Kincaid Park", "Anchorage, AK", "You could walk to Fire Island when the tide is down but you might die LOL"),
+    ("FlatTop Trail", "Anchorage, AK", "Flattop Trail is the best trail in Anchorage. Well treaded. Very public"),
+    ("Byron Glacier Trail", "Portage, AK", "Easy walk to some ice caves ooh la la!"),
+    ("Portage Glacier Trail", "Portage, AK", "Ha literally have to get here from Whittier and it depends on the weather LOL")
+    ])
+def test_submit_a_trail(browser_session, input_name, input_location, input_description):
+    browser=browser_session
+
+    browser.get('http://127.0.0.1:5000/add')
+
+    name_input = browser.find_element(By.ID,"inputName")
+    location_input = browser.find_element(By.ID, "inputLocation")
+    description_input = browser.find_element(By.ID, "inputDescription")
+    submit_button = browser.find_element(By.TAG_NAME, "button")
+    
+    name_input.send_keys(input_name)
+    location_input.send_keys(input_location)
+    description_input.send_keys(input_description)
+    submit_button.click()
+
+    success_message = browser.find_element(By.ID, "statusmessage")
+
+    assert success_message is not None, "Trail Information Not Submitted Successfully"
